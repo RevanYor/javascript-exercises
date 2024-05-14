fb258a4548e271b9cb9a5fa2db034fd5203597f0
RevanYor <revansk@protonmail.com>
2024-05-14 19:00:32 -0400

    Excercises  1 -3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let change = string.replaceAll(',','?');
+    let word = change.split('');
+    let reversed = word.reverse();
+    let final = reversed.toString();
+    let con = final.replaceAll(',',"");
+    let done = con.replaceAll('?',',');
+    return done;
 };
 
 // Do not edit below this line
