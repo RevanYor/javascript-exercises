c9f506c7156568471d261f2dc27a5bb2717c4519
RevanYor <revansk@protonmail.com>
2024-05-15 16:07:30 -0400

    Still on excersice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function(array,arg) {
-    array.filter(arg);
-};
+const removeFromArray = function(array,...elem) {
 
+    let arr = [...elem];
+    for (let i = arr.length; i < arr.length; i++) {
+        const index = array.indexOf(elem);
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
+            return array;
+    }
+};
 // Do not edit below this line
 module.exports = removeFromArray;
