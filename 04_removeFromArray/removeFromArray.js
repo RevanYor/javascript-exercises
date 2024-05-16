0a77ed28a87cfc06668c627e052d1f47d52d103e
RevanYor <revansk@protonmail.com>
2024-05-16 12:23:47 -0400

    Indentation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,11 +3,11 @@ const removeFromArray = function(array,...elem) {
     let arr = [...elem];
     arr.forEach((elem)=> {
         for(let i = 0; i < array.length; i++) {
-        const index = array.indexOf(elem);
-        if (index !== -1) {
-            array.splice(index,1);
+            const index = array.indexOf(elem);
+            if (index !== -1) {
+                array.splice(index,1);
+            }
         }
-    }
     })
     return array;
 };
