ea4dd45fbd11632ba5b26f01fc62dce68166da18
RevanYor <revansk@protonmail.com>
2024-05-16 12:21:25 -0400

    Excercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,10 +2,12 @@ const removeFromArray = function(array,...elem) {
 
     let arr = [...elem];
     arr.forEach((elem)=> {
+        for(let i = 0; i < array.length; i++) {
         const index = array.indexOf(elem);
         if (index !== -1) {
             array.splice(index,1);
         }
+    }
     })
     return array;
 };
