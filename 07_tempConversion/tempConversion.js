9cc8a60f61e57a51e808246a1ba8e4c42bec05ac
RevanYor <revansk@protonmail.com>
2024-05-17 10:57:17 -0400

    Excercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32) * (5/9);
+  let rounded = Math.round(cel*10)/10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let far = (temp * (9/5) +32);
+  let rounded = Math.round(far*10)/10;
+  return rounded;
 };
 
 // Do not edit below this line
