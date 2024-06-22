475ae6d2a2720e7a9d1f3d0349dac5f0a30eb870
RevanYor <revansk@protonmail.com>
2024-06-22 16:07:50 -0400

    Calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = array.reduce((sum, total) => sum + total, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multed = array.reduce((mul, item) => mul * item);
+  return multed;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let start = num;
+  let total = [];
+  for (let i = 0; i < num; i++) {
+    let done = start - 1;
+    start--
+    total.push(done);
+  }
+  total.pop();
+  let doe = total.reduce((facto, item) => facto * item, num);
+  if( num === 0) {
+    return 1;
+  } else {
+    return doe;
+  }
 };
 
 // Do not edit below this line
