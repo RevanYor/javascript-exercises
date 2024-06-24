89baab9b79605d9bcaa626f2d049eb20ecce88f2
RevanYor <revansk@protonmail.com>
2024-06-23 23:59:36 -0400

    Started fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let tran = Number(num);
+    let fib = 1;
+    let total = 0;
+    let arr = [];
+    if(tran > 0) {
+        for(let i = 0; 2 > i; i++) {
+            total = tran -1 ;
+            arr.push(total);
+            tran--
+        }
+        return arr;
+    }
 };
 
 // Do not edit below this line
