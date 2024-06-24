828c73ae6ede3173c2a525f711930cbcd48dc44a
RevanYor <revansk@protonmail.com>
2024-06-23 23:22:10 -0400

    Palindromes not finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let arr = word.toLowerCase();
+    let array = arr.split(',');
+    let arr2 = array.slice();
+    let array2 = arr2.reverse();
+    let jon = array.join(',');
+    let jon2 = array2.join(',');
+    /* if (array.length !== array2.length) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] == array2[i]) {
+                return true;
+            }
+        }
+    } 
+    return array2; */
+    if(jon == jon2) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
